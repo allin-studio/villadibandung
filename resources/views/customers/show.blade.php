831f8643c6b55ca27ad8ca8798671f5203c6f1f9
aditrio <30noadi@gmail.com>
2023-08-18 18:26:39 +0700

    25

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -68,7 +68,7 @@
 		<div class="container">
 			<div class="menu-bg-wrap">
 				<div class="site-navigation">
-					<a href="vilas" class="logo m-0 float-start">Villa di bandung.</a>
+					<a href="25" class="logo m-0 float-start">Villa di bandung.</a>
 
 					<ul class="js-clone-nav d-none d-lg-inline-block text-start site-menu float-end">
 						<li class="active"><a href="{{ route('customers.index')}}">Kembali</a></li>
