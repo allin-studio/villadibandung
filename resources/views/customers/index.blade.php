222599b71938c73f518664158220b498dcbe969e
imigransorga <rizkyayahya12@gmail.com>
2023-08-18 11:48:55 +0700

    update link

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -360,7 +360,7 @@
 				<div class="col-lg-4">
 					<div class="widget">
 						<h3>Contact</h3>
-						<address>Jl. Ir Djuanda, Kota Bandung dago</address>
+						<address>Jl. Permata Permai XII No. 44, Cisaranten Kulon Arcamanik Bandung</address>
 						<ul class="list-unstyled links">
 							<li><a href="tel://11234567890">+62(022)-456-7890</a></li>
 							<li><a href="tel://11234567890">+62 85462548526</a></li>
@@ -370,38 +370,22 @@
 				</div> <!-- /.col-lg-4 -->
 				<div class="col-lg-4">
 					<div class="widget">
-						<h3>Sources</h3>
-						<ul class="list-unstyled float-start links">
-							<li><a href="#">About us</a></li>
-							<li><a href="#">Services</a></li>
-
-						</ul>
-						<ul class="list-unstyled float-start links">
-							<li><a href="#">Partners</a></li>
-							<li><a href="#">Business</a></li>
-
-						</ul>
-					</div> <!-- /.widget -->
-				</div> <!-- /.col-lg-4 -->
-				<div class="col-lg-4">
-					<div class="widget">
 						<h3>Links</h3>
 						<ul class="list-unstyled links">
-							<li><a href="#">About us</a></li>
-							<li><a href="#">Contact us</a></li>
+							<ul class="list-unstyled links">
+								<li><a href="https://www.tokopedia.com/villabandungamethys">Tokopedia</a></li>
+								<li><a href="https://shopee.co.id/jiwaramadhan">Shopee</a></li>
+								<li><a href="https://www.facebook.com/amethystdago/">Facebook</a></li>
+								<li><a href="https://www.airbnb.co.id/users/show/186407489">airbnb</a></li>
+							</ul>
 						</ul>
-
 						<ul class="list-unstyled social">
-							<li><a href="#"><span class="icon-instagram"></span></a></li>
-							<li><a href="#"><span class="icon-twitter"></span></a></li>
-							<li><a href="#"><span class="icon-facebook"></span></a></li>
-							<li><a href="#"><span class="icon-linkedin"></span></a></li>
-							<li><a href="#"><span class="icon-pinterest"></span></a></li>
-							<li><a href="#"><span class="icon-dribbble"></span></a></li>
+							<li><a href="https://www.facebook.com/amethystdago/"><span class="icon-facebook"></span></a></li>
+							<li><a href="https://www.pinterest.com/adagoresort/"><span class="icon-pinterest"></span></a></li>
 						</ul>
 					</div> <!-- /.widget -->
 				</div> <!-- /.col-lg-4 -->
-			</div> <!-- /.row -->
+
 
 			<div class="row mt-5">
 				<div class="col-12 text-center">
