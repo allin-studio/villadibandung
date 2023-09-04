d21bfc951c86a77fa61ed395de6b8346b13c8408
edvinmegantara9 <edvin.megantara9@gmail.com>
2023-09-04 18:06:21 +0700

    teset

--- app/Models/TransaksiBooking.php
+++ app/Models/TransaksiBooking.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class TransaksiBooking extends Model
+class TransaksiBooking extends Model 
 {
     protected $fillable = [
         'id_villa',
