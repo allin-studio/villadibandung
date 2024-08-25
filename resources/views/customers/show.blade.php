daa1cd449f86a9b6f711ae0473d0979bef0d30f7
rachmaliaputris <rachmalia.putri@mhs.itenas.ac.id>
2024-08-25 21:16:29 +0700

    remove button online booking

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -144,7 +144,7 @@
 					<div id="map">
 
                     <a href="{{ route('createBookingForm', ['id' => $vila->id]) }}" class="btn btn-primary">Manual Booking</a>
-                    <a href="{{ route('reservation.details', ['id' => $vila->id]) }}" class="btn btn-primary">Online Booking</a>
+                    {{--  <a href="{{ route('reservation.details', ['id' => $vila->id]) }}" class="btn btn-primary">Online Booking</a>  --}}
                     {{--  <a href="{{ route('reservation.form') }}" class="btn btn-primary">Online Booking</a>  --}}
 
 				<!--
